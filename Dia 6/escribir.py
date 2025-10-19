638bae2465820579873cee0a9e935a7f4894cff6
Alcala <Juan.Alcala@btgpactual.com>
2025-10-19 15:56:29 -0500

    Cambios sobre el dia 6 correcion a nombramientos y correciones de sintaxis

--- Dia 6/escribir.py
+++ Dia 6/escribir.py
@@ -1,5 +1,5 @@
 mi_Archivo=open('prueba.txt','w')
-mi_Archivo.write('Hola como estas ')
+mi_Archivo.write=input("Escribe algo")
 
 
 
---
+++ Dia 6/salida_entrada.py
@@ -0,0 +1,3 @@
+mi_Archivo=open('prueba.txt')
+print(mi_Archivo)
+print(mi_Archivo.read())
